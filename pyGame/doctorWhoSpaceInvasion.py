6295af8c261d84bac1d259d6295731151c435fee
Harrison, Robson (Contractor-NBCUniversal) <Robson.Harrison@nbcuni.com>
2025-12-05 15:32:07 +0000

    - Implemented the Barrier subclass - Implemented the Laser class
    Co-authored-by: abdulmateenahmed02-dot <abdulmateenahmed02-dot@users.noreply.github.com>

--- pyGame/doctorWhoSpaceInvasion.py
+++ pyGame/doctorWhoSpaceInvasion.py
@@ -76,6 +76,77 @@ class Defender(Entity):
         """Get the starting position for a laser fired by the defender (centre of the defender)"""
         return (self.x + self.width // 2, self.y)
 
+class Barrier(Entity):
+    """Class representing a barrier"""
+
+    def init(self, name, x, y, spriteFile, width=100, height= 24, maxHealth=3):
+        """Initialise barrier with given properties"""
+        super().init(name, x, y, width, height)
+
+        self.health = maxHealth
+        self.maxHealth = maxHealth
+
+        spritePath = os.path.join(gameDirectory, spriteFile)
+        sprite = pygame.image.load(spritePath)
+        self.sprite = pygame.transform.scale(sprite, (self.width, self.height))
+        self.image = self.sprite.copy()
+        self.damageRegions = []
+
+    def takeDamage(self):
+        """Reduce barrier health and update damage regions"""
+        self.health -= 1
+
+        # Create damage regions
+        if not self.isDestroyed():
+            for _ in range(random.randint(3, 6)):
+                damageX = random.randint(5, self.width - 5)
+                damageY = random.randint(2, self.height - 2)
+                damageSize = random.randint(4, 10)
+                self.damageRegions.append((damageX, damageY, damageSize))
+
+            self.image = self.sprite.copy()
+
+            for damageX, damageY, damageSize in self.damageRegions:
+                pygame.draw.circle(self.image, (0,0,0), damageX, damageY, damageSize)
+
+    def isDestroyed(self):
+        """Check if the barrier is destroyed"""
+        return self.health <= 0
+
+    def draw(self, screen):
+        """Draw the barrier on the screen"""
+        if not self.isDestroyed():
+            screen.blit(self.image, (self.x, self.y))
+
+class Laser:
+    """Class representing a laser"""
+
+    def init(self, x, y, speed, colour, width=5, height=10):
+        """Initialise laser with given properties"""
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.colour = colour
+        self.width = width
+        self.height = height
+
+    def move(self):
+        """Move the laser"""
+        self.y += self.speed
+
+    def getRect(self):
+        """Get the rectangle representing the laser's position and size"""
+        return pygame.Rect(self.x, self.y, self.width, self.height)
+
+    def draw(self, screen):
+        """Draw the laser on the screen"""
+        pygame.draw.rect(screen, self.colour, (self.x, self.y, self.width, self.height))
+    
+    def isOffScreen(self, displayHeight):
+        """Check if the laser is off the screen"""
+        return self.y < 0 or self.y > displayHeight
+
+
 screen = pygame.display.set_mode((displayWidth, displayHeight))
 pygame.display.set_caption("Doctor Who Space Invasion")
 clock = pygame.time.Clock()
